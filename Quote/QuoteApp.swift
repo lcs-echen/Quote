53aba9148dbd412f4c2c22b05d51074a5a74fbc4
Evelyn(Yuyang) Chen <yychen@lcs.on.ca>
2023-04-14 14:58:49 -0400

    Initial Commit

---
+++ Quote/QuoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuoteApp.swift
+//  Quote
+//
+//  Created by Evelyn Chen on 2023-04-14.
+//
+
+import SwiftUI
+
+@main
+struct QuoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
